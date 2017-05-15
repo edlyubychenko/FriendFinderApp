fd475882a2139256a9a10427c27542574274235f
Ebony Davis <you@example.com>
2017-05-15 14:55:58 -0700

    idk

---
+++ friends.js
@@ -0,0 +1,12 @@
+var defaultFriends = [{
+  friendName: "dog",
+  photoURL: "http://imgur.com/gallery/A8eQsll"
+  questionArray: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+  },
+  {
+    friendName: "rabbit"
+    photoURL: "http://imgur.com/gallery/ePbPAFo",
+    questionArray: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5],
+  },
+  }
+module.exports = defaultFriends
